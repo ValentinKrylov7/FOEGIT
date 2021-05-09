91281ba01b1da9b4e94933f05532b0985084fa24
Valentin <valent1n.krylov@yandex.ru>
2021-05-10 02:26:34 +0300

    Second Commit

---
+++ Assets/ColorScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour
+{
+    Vector3 scale = new Vector3(5f, 5f, 5f);
+    Vector3 scale1 = new Vector3(1f, 1f, 1f);
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale = scale;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = scale1;
+    }
+}
